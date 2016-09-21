61015233be91688db9fc458b5d724135c838a80c
zhengzy <zhezhiyong@163.com>
2016-09-21 20:39:50 +0800

    add:spel自定义key

--- src/main/java/com/cache/me/service/DemoService.java
+++ src/main/java/com/cache/me/service/DemoService.java
@@ -1,6 +1,7 @@
 package com.cache.me.service;
 
 import com.cache.me.entity.User;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
@@ -8,24 +9,37 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * @desc: 服务数据提供
- * @author: zhengzy
- * @date: 2016/8/12
+ * desc: 服务数据提供
+ * author: zhengzy
+ * date: 2016/8/12
  */
 @Service
 public class DemoService {
 
-    @Cacheable(value = "usercache", keyGenerator = "KeyGenerator")
+    @Cacheable(value = "userCache", keyGenerator = "KeyGenerator")
     public User findUser(Long id, String firstName, String lastName) {
         System.out.println("db qry");
         return new User(id, firstName, lastName);
     }
-    @Cacheable(value = "userlistcache", keyGenerator = "KeyGenerator")
+
+    @Cacheable(value = "userListCache", keyGenerator = "KeyGenerator")
     public List<User> findUserList(Long companyId) {
         System.out.println("db qry list");
         List<User> list = new ArrayList<>();
-        list.add(new User(1l,"zheng", "zhiyong"));
-        list.add(new User(2l,"xiao", "ming"));
+        list.add(new User(1L, "zheng", "zhiyong"));
+        list.add(new User(2L, "xiao", "ming"));
         return list;
+    }
+
+    @Cacheable(value = "userCacheById", key = "'key:'+#id.toString()")
+    public User findUserById(Long id) {
+        System.out.println("db custom qry");
+        return new User(id, "xiao", "ming");
+    }
+
+    @CacheEvict(value = "userCacheById", key = "'key:'+#id.toString()")
+    public int delUserById(Long id) {
+        System.out.println("db custom del");
+        return 1;
     }
 }
--- src/main/java/com/cache/me/web/DemoController.java
+++ src/main/java/com/cache/me/web/DemoController.java
@@ -10,9 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 /**
- * @desc: 控制器
- * @author: zhengzy
- * @date: 2016/8/12
+ * desc: 控制器
+ * author: zhengzy
+ * date: 2016/8/12
  */
 @RestController
 public class DemoController {
@@ -22,17 +22,32 @@ public class DemoController {
 
     @RequestMapping(value = "/test", method = RequestMethod.GET)
     public String testCache() {
-        User user = demoService.findUser(1l, "first", "last");
+        User user = demoService.findUser(1L, "first", "last");
         System.out.println("user:" + "/" + user.getFirstName() + "/" + user.getLastName());
         return "ok";
     }
 
     @RequestMapping(value = "/testList", method = RequestMethod.GET)
     public String testCacheList(){
-        List<User> list = demoService.findUserList(1l);
+        List<User> list = demoService.findUserList(1L);
         System.out.println(list.size());
         return "list";
     }
+
+    @RequestMapping(value = "/testId", method = RequestMethod.GET)
+    public String testById() {
+        User user = demoService.findUserById(1L);
+        System.out.println("user:" + "/" + user.getFirstName() + "/" + user.getLastName());
+        return "testById";
+    }
+
+    @RequestMapping(value = "/testDelId", method = RequestMethod.GET)
+    public String testDelId() {
+        int result = demoService.delUserById(1L);
+        System.out.println("testDelId:" + result);
+        return "testDelId";
+    }
+
 
 
 
