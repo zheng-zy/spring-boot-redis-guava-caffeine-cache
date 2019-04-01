dc6533f0d86557ab6de06f70ebfbc0dbd1e579ba
zhengzy <zhezhiyong@163.com>
2019-04-01 13:54:52 +0800

    spring boot 2 多实例redis缓存

--- spring-boot2-redis-cache/src/main/java/com/zy/config/RedisConfig.java
+++ spring-boot2-redis-cache/src/main/java/com/zy/config/RedisConfig.java
@@ -1,22 +1,33 @@
 package com.zy.config;
 
 import com.alibaba.fastjson.support.spring.GenericFastJsonRedisSerializer;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Scope;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettucePoolingClientConfiguration;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import java.time.Duration;
@@ -30,55 +41,106 @@ import java.time.Duration;
 @Slf4j
 public class RedisConfig extends CachingConfigurerSupport {
 
+    @Value("${spring.application.name:unknown}")
+    private String appName;
+    @Value("${spring.redis.timeToLive:15}")
+    private Long timeToLive;
+
     @Bean
     @ConfigurationProperties(prefix = "spring.redis.lettuce.pool")
     @Scope(value = "prototype")
-    public GenericObjectPoolConfig redisPool(){
+    public GenericObjectPoolConfig redisPool() {
         return new GenericObjectPoolConfig();
     }
 
     @Bean
     @ConfigurationProperties(prefix = "spring.redis.redis-a")
-    public RedisStandaloneConfiguration redisConfigA(){
+    public RedisStandaloneConfiguration redisConfigA() {
         return new RedisStandaloneConfiguration();
     }
 
     @Bean
     @ConfigurationProperties(prefix = "spring.redis.redis-b")
-    public RedisStandaloneConfiguration redisConfigB(){
+    public RedisStandaloneConfiguration redisConfigB() {
         return new RedisStandaloneConfiguration();
     }
 
     @Bean
     @Primary
-    public LettuceConnectionFactory factoryA(GenericObjectPoolConfig config, RedisStandaloneConfiguration redisConfigA){
+    public LettuceConnectionFactory factoryA(GenericObjectPoolConfig config, RedisStandaloneConfiguration redisConfigA) {
         LettuceClientConfiguration clientConfiguration = LettucePoolingClientConfiguration.builder()
                 .poolConfig(config).commandTimeout(Duration.ofMillis(config.getMaxWaitMillis())).build();
         return new LettuceConnectionFactory(redisConfigA, clientConfiguration);
     }
 
     @Bean
-    public LettuceConnectionFactory factoryB(GenericObjectPoolConfig config, RedisStandaloneConfiguration redisConfigB){
+    public LettuceConnectionFactory factoryB(GenericObjectPoolConfig config, RedisStandaloneConfiguration redisConfigB) {
         LettuceClientConfiguration clientConfiguration = LettucePoolingClientConfiguration.builder()
                 .poolConfig(config).commandTimeout(Duration.ofMillis(config.getMaxWaitMillis())).build();
         return new LettuceConnectionFactory(redisConfigB, clientConfiguration);
     }
 
     @Bean(name = "redisTemplateA")
-    public StringRedisTemplate redisTemplateA(LettuceConnectionFactory factoryA){
-        StringRedisTemplate template = getRedisTemplate();
-        template.setConnectionFactory(factoryA);
+    public StringRedisTemplate redisTemplateA(LettuceConnectionFactory factoryA) {
+        StringRedisTemplate template = new StringRedisTemplate(factoryA);
+        RedisSerializer<String> redisSerializer = new StringRedisSerializer();
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+        ObjectMapper om = new ObjectMapper();
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        jackson2JsonRedisSerializer.setObjectMapper(om);
+        template.setKeySerializer(redisSerializer);
+        template.setValueSerializer(jackson2JsonRedisSerializer);
+        template.setHashValueSerializer(jackson2JsonRedisSerializer);
         return template;
     }
 
     @Bean(name = "redisTemplateB")
-    public StringRedisTemplate redisTemplateB(@Autowired@Qualifier("factoryB") LettuceConnectionFactory factoryB){
-        StringRedisTemplate template = getRedisTemplate();
-        template.setConnectionFactory(factoryB);
+    public StringRedisTemplate redisTemplateB(@Autowired @Qualifier("factoryB") LettuceConnectionFactory factoryB) {
+        StringRedisTemplate template = new StringRedisTemplate(factoryB);
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+        ObjectMapper om = new ObjectMapper();
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        jackson2JsonRedisSerializer.setObjectMapper(om);
+        template.setValueSerializer(jackson2JsonRedisSerializer);
+        template.afterPropertiesSet();
         return template;
     }
 
-    private StringRedisTemplate getRedisTemplate(){
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory factory) {
+        RedisSerializer<String> redisSerializer = new StringRedisSerializer();
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+
+        // 配置序列化
+        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(timeToLive))
+                .prefixKeysWith(appName + ":");
+        RedisCacheConfiguration redisCacheConfiguration = config.serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(redisSerializer))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(jackson2JsonRedisSerializer));
+        RedisCacheManager cacheManager = RedisCacheManager.builder(factory)
+                .cacheDefaults(redisCacheConfiguration)
+                .build();
+        return cacheManager;
+    }
+
+    //        RedisCacheConfiguration configuration = RedisCacheConfiguration.defaultCacheConfig();
+//        configuration.prefixKeysWith("bda");
+//        configuration = configuration.entryTtl(Duration.ofMinutes(30));
+//        Set<String> cacheNames = new HashSet<>();
+//        cacheNames.add("test1");
+//        cacheNames.add("test2");
+//        Map<String, RedisCacheConfiguration> configurationMap = new HashMap<>(10);
+//        configurationMap.put("test1", configuration);
+//        configurationMap.put("test2", configuration.entryTtl(Duration.ofMinutes(60)));
+//        RedisCacheManager manager = RedisCacheManager.builder(factory)
+//                .initialCacheNames(cacheNames)
+//                .withInitialCacheConfigurations(configurationMap)
+//                .build();
+//        return manager;
+
+    private StringRedisTemplate getRedisTemplate() {
         StringRedisTemplate template = new StringRedisTemplate();
         template.setValueSerializer(new GenericFastJsonRedisSerializer());
         template.setValueSerializer(new StringRedisSerializer());
